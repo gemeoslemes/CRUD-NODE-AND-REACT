a64f418acbf3484e07eb50a2ad31e6f3106f0ac5
gemeoslemes <victorlemes0776@gmail.com>
2024-05-29 19:51:12 -0300

    Adicionado arquivos no diretório frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,52 @@
+import Global from "./styles/global.js";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+import Grid from "./components/Grid.js";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 800px;
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+`;
+
+const Title = styled.h2``;
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error.message || "Erro ao buscar usuários.");
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Title>USUÁRIOS</Title>
+        <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
+        <Grid users={users} setUsers={setUsers} setOnEdit={setOnEdit} />
+      </Container>
+      <ToastContainer autoClose={3000} position="bottom-left" closeOnClick />
+      <Global />
+    </>
+  );
+}
+
+export default App;
